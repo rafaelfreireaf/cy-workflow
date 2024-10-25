a29ad189784dc9cad87dffe9f4a1352c1b40dcaa
Rafael Freire <rafael.freire@arenafantasy.com>
2024-10-25 14:22:17 -0300

    Test develop

--- __tests__/e2e/app.cy.ts
+++ __tests__/e2e/app.cy.ts
@@ -2,5 +2,6 @@ describe('App', () => {
   it('should render the home page', () => {
     cy.visit('/')
     cy.contains('h1', 'Welcome to Nuxt!')
+    cy.contains('h2', 'Testing with Cypress')
   })
 })
--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
     <h1>Welcome to Nuxt!</h1>
+    <h2>Testing with Cypress</h2>
   </div>
 </template>
